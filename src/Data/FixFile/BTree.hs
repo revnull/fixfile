83f27f35797349321eae071eb750e151bc5b284d
Rev. Johnny Healey <rev.null@gmail.com>
2016-02-11 23:47:19 -0500

    Made some improvements to the BTree.
    The branch size is now specified at the type level. Arrays were
    replaced with vectors, with some general code cleanup.
    
    Faster unit tests.

--- src/Data/FixFile/BTree.hs
+++ src/Data/FixFile/BTree.hs
@@ -1,5 +1,5 @@
 {-# LANGUAGE DeriveGeneric, DeriveFunctor, DeriveFoldable, DeriveTraversable,
-    DeriveDataTypeable #-}
+    DeriveDataTypeable, DataKinds, KindSignatures #-}
 
 {- |
     Module      :  Data.FixFile.BTree
@@ -17,6 +17,7 @@ module Data.FixFile.BTree (BTree
                           ,createBTreeFile
                           ,openBTreeFile
                           ,empty
+                          ,depth
                           ,insertBTree
                           ,insertBTreeT
                           ,lookupBTree
@@ -25,231 +26,297 @@ module Data.FixFile.BTree (BTree
                           ,filterBTreeT
                           ,deleteBTree
                           ,deleteBTreeT
+                          ,partitionBTree
                           ,toListBTree
                           ,fromListBTree
                           ) where
 
-import Data.Array
+import Control.Monad.Writer
 import Data.Binary
 import Data.Dynamic
+import qualified Data.Vector as V
 import GHC.Generics
+import GHC.TypeLits
 
 import Data.FixFile
 
 {- |
-    A 'Fixed' @('BTree' k v)@ stores a BTree of key/value pairs.
+    A 'Fixed' @('BTree' n k v)@ stores a BTree of key/value pairs.
+    'n' should be a 'Nat' and will be the maximum number of elements in each
+    branch of the 'BTree'.
 -}
-data BTree k v a =
+data BTree (n :: Nat) k v a =
     Empty
   | Value v
-  | Node Word32 (Array Int (k, a))
+  | Node Word32 (V.Vector (k, a))
     deriving (Read, Show, Generic, Functor, Foldable, Traversable, Typeable)
 
-instance (Binary k, Binary v, Binary a) => Binary (BTree k v a)
+instance (Binary k, Binary v, Binary a) => Binary (BTree n k v a) where
+    put Empty = putWord8 0x45
+    put (Value v) = putWord8 0x56 >> put v
+    put (Node d vec) = do
+        putWord8 0x4e
+        put d
+        put (V.length vec)
+        mapM_ put vec
+    get = getWord8 >>= getBTree where
+        getBTree 0x45 = return Empty
+        getBTree 0x56 = Value <$> get
+        getBTree 0x4e = Node <$> get <*> (get >>= \n -> V.replicateM n get)
+        getBTree _ = error "Can't decode into BTree"
+
+-- | Compute the depth of a 'BTree' 
+depth :: Fixed g => g (BTree n k v) -> Int
+depth = cata phi where
+    phi Empty = 0
+    phi (Value _) = 1
+    phi (Node d _) = fromIntegral $ 1 + d
 
 -- | An empty 'BTree' 
-empty :: Fixed g => g (BTree k v)
+empty :: Fixed g => g (BTree n k v)
 empty = inf Empty
 
-value :: Fixed g => v -> g (BTree k v)
+value :: Fixed g => v -> g (BTree n k v)
 value = inf . Value
 
-node :: Fixed g => Word32 -> Array Int (k, g (BTree k v)) -> g (BTree k v)
+node :: Fixed g => Word32 -> V.Vector (k, g (BTree n k v)) -> g (BTree n k v)
 node d = inf . Node d
 
 -- | Create a 'FixFile' storing a @('BTree' k v)@.
 --   The initial value is 'empty'.
-createBTreeFile :: (Binary k, Typeable k, Binary v, Typeable v) =>
-    FilePath -> IO (FixFile (Ref (BTree k v)))
+createBTreeFile :: (Typeable n, Binary k, Typeable k, Binary v, Typeable v) =>
+    FilePath -> IO (FixFile (Ref (BTree n k v)))
 createBTreeFile fp = createFixFile (Ref empty) fp
 
 -- | Open a 'FixFile' storing a @('BTree' k v)@.
 openBTreeFile :: (Binary k, Typeable k, Binary v, Typeable v) =>
-    FilePath -> IO (FixFile (Ref (BTree k v)))
+    FilePath -> IO (FixFile (Ref (BTree n k v)))
 openBTreeFile = openFixFile
 
-nodeSize :: Integral i => i
-nodeSize = 32
+treeNodeSize :: KnownNat n => g (BTree n k v) -> Integer
+treeNodeSize = validate . natVal . p where
+    p :: g (BTree n k v) -> Proxy n
+    p _ = Proxy
+    validate n = if n < 2
+        then error "BTree branch size must be > 1."
+        else n
 
-lookupPos :: (Ord k) => Bool -> k -> Array Int (k, v) ->
-    (Int, [(k, v)], (k, v), [(k, v)])
-lookupPos ff k arr = result . findFirst . uncurry binary $ bounds arr where
-    result i =
-        let (a, b:c) = splitAt i $ elems arr
-        in (i, a, b, c)
-    lookupi = fst . (arr !)
-    findFirst = if ff then findFirst' else id
-    findFirst' 0 = 0
-    findFirst' i = if lookupi (i - 1) == k
-        then findFirst' (i - 1)
-        else i
-    binary mini maxi = 
-        let avg = (maxi + mini) `div` 2
-            avgi = lookupi avg
-        in case (maxi - mini <= 1, compare k avgi) of 
-            (True, _) -> if lookupi maxi <= k then maxi else mini
-            (_, EQ) -> avg
-            (_, LT) -> binary mini (avg - 1)
-            (_, _) -> binary avg maxi
+splitRange :: Ord k => k -> V.Vector (k, v) -> (Int, Int)
+splitRange k vec = V.foldl' rangeSum (0,0) vec where
+    rangeSum t@(i1, i2) (k', _)
+        | k' < k = (i1 `seq` i1 + 1, i2 `seq` i2 + 1)
+        | k == k' = (i1, i2 `seq` i2 + 1)
+        | otherwise = t
 
-splitRange :: (Ord k) => k -> Array Int (k, v) ->
-    ([(k,v)], [(k,v)], [(k,v)])
-splitRange k = uncurry splitMax . splitMin id Nothing . elems where
-    splitMin f Nothing [] = (f [], [])
-    splitMin f (Just t) [] = (f [], [t])
-    splitMin f Nothing xl@(xt@(xk,_):xs) = case compare xk k of
-        LT -> splitMin f (Just xt) xs
-        _ -> (f [], xl)
-    splitMin f (Just t) xl@(xt@(xk,_):xs) = case compare xk k of
-        LT -> splitMin (f . (t:)) (Just xt) xs
-        _ -> (f [], t:xl)
-    splitMax p xs = 
-        let (c, n) = splitMax' id xs
-        in (p, c, n)
-    splitMax' f [] = (f [], [])
-    splitMax' f xl@(xt@(xk,_):xs) = case compare xk k of
-        GT -> (f [], xl)
-        _ -> splitMax' (f . (xt:)) xs
+split3 :: (Int, Int) -> V.Vector a -> (V.Vector a, V.Vector a, V.Vector a)
+split3 (s1, s2) vec = (vl, vm, vr) where
+    (vm',vr) = V.splitAt s2 vec
+    (vl, vm) = V.splitAt s1 vm'
 
-data Insert k v g =
-    Inserted k (g (BTree k v))
-  | Split Word32 (k, (g (BTree k v))) (k, (g (BTree k v)))
+data Insert n k v g =
+    Inserted k (g (BTree n k v))
+  | Split Word32 (k, (g (BTree n k v))) (k, (g (BTree n k v)))
 
 -- | Insert the value 'v' with the key 'k' into a 'Fixed' @('BTree' k v)@.
-insertBTree :: (Ord k, Fixed g) => k -> v -> g (BTree k v) -> g (BTree k v)
-insertBTree k v = merge . para phi where
+insertBTree :: (KnownNat n, Ord k, Fixed g) => k -> v -> g (BTree n k v) ->
+    g (BTree n k v)
+insertBTree k v t = merge . para phi $ t where
     merge (Inserted _ x) = x
-    merge (Split d lt rt) = node (d + 1) $ array (0, 1)
-        [(0, lt), (1, rt)]
-    
-    newNode d c ls = if c > nodeSize
-        then
-            let (l, r) = splitAt half ls
-                half = nodeSize `div` 2
-                half' = c - half
-                mini = fst . head
-            in Split d (mini l, node d $ array (0, half - 1) $ zip [0..] l)
-                (mini r, node d $ array (0, half' - 1) $ zip [0..] r)
-        else Inserted (fst $ head ls) (node d $ array (0, c-1) $ zip [0..] ls)
+    merge (Split d lt rt) = node (d + 1) $ V.fromList [lt, rt]
+    nodeSize = fromIntegral $ treeNodeSize t
 
-    children xs = [(i, x) | (i, (x, _)) <- xs]
+    newNode d c cs
+        | c > nodeSize =
+            let (l, r) = V.splitAt (nodeSize `div` 2) cs
+                l' = V.force l
+                r' = V.force r
+                mini = fst . V.head
+            in Split d (mini l, node d l') (mini r, node d r')
+        | otherwise = 
+            Inserted (fst $ V.head cs) (node d cs)
 
-    phi Empty = Inserted k $ node 0 $ array (0,0) [(0, (k, value v))]
+    nodes = fmap (\(a,(b,_)) -> (a, b))
+
+    phi Empty = Inserted k $ node 0 $ V.singleton (k, value v)
     phi (Value _) = error "insertBTree phi Value error"
-    phi (Node 0 a) =
-        let (_, p, (kc, (km, _)), n) = lookupPos False k a
-            newSize = (2+) . snd . bounds $ a
-        in if kc <= k
-            then newNode 0 newSize $
-                children p ++ [(kc, km), (k, value v)] ++ children n
-            else newNode 0 newSize $
-                children p ++ [(k, value v), (kc, km)] ++ children n
-    phi (Node d a) =
-        let (_, p, (_, (_, ka)), n) = lookupPos False k a
-            newSize = 1 + currSize
-            currSize = (1+) . snd . bounds $ a
-        in case ka of
-            Inserted k' n' -> newNode d currSize $
-                children p ++ (k', n'):children n
-            Split _ lt rt -> newNode d newSize $
-                children p ++ [lt, rt] ++ children n
+
+    phi (Node 0 vec) =
+        let (lt, eq, gt) = split3 (splitRange k vec) (nodes vec)
+            newSize = 1 + V.length vec
+        in newNode 0 newSize (V.concat [lt, eq, V.singleton (k, value v), gt])
+    phi (Node d vec) = 
+        let (lt, eq, gt) = split3 (splitRange k vec) vec
+            lt' = nodes lt
+            eq' = nodes eq
+            gt' = nodes gt
+            currSize = V.length vec
+            (c, csf) = case (V.null eq, V.null lt) of
+                (False, _) ->
+                    (V.last eq, \n -> V.concat [lt', V.init eq', n, gt'])
+                (_, False) ->
+                    (V.last lt, \n -> V.concat [V.init lt', n, gt'])
+                _ -> (V.head gt, \n -> V.concat [n, V.tail gt'])
+        in case snd (snd c) of
+            Inserted k' n' ->
+                newNode d currSize (csf $ V.singleton (k', n'))
+            Split _ ls rs ->
+                newNode d (currSize + 1) (csf $ V.fromList [ls, rs])
 
 -- | 'Transaction' version of 'insertBTree'.
-insertBTreeT :: (Ord k, Binary k, Binary v) => k -> v ->
-    Transaction (Ref (BTree k v)) s ()
+insertBTreeT :: (KnownNat n, Ord k, Binary k, Binary v) => k -> v ->
+    Transaction (Ref (BTree n k v)) s ()
 insertBTreeT k v = alterT (insertBTree k v)
 
 -- | Lookup the values stored for the key 'k' in a 'Fixed' @('BTree' k v)@.
-lookupBTree :: (Ord k, Fixed g) => k -> g (BTree k v) -> [v]
+lookupBTree :: (Ord k, Fixed g) => k -> g (BTree n k v) -> [v]
 lookupBTree k = ($ []) . cata phi where
     phi Empty l = l
     phi (Value v) l = v:l
-    phi (Node 0 a) l = foldr ($) l . fmap snd . filter ((k ==) . fst) . elems
-        $ a
-    phi (Node _ a) l =
-        let (_, c, _) = splitRange k a
-        in foldr ($) l $ fmap snd c
+    phi (Node 0 vec) l =
+        let (_, eq, _) = split3 (splitRange k vec) vec
+        in V.foldr (($) . snd) l eq
+    phi (Node _ vec) l =
+        let (_, eq, _) = split3 (s1 - 1, s2) vec
+            (s1, s2) = splitRange k vec
+        in V.foldr (($) . snd) l eq
 
 -- | 'Transaction' version of 'lookupBTree'.
 lookupBTreeT :: (Ord k, Binary k, Binary v) => k ->
-    Transaction (Ref (BTree k v)) s [v]
+    Transaction (Ref (BTree n k v)) s [v]
 lookupBTreeT k = lookupT (lookupBTree k)
 
-data Deleted k v g =
-    Deleted k (g (BTree k v))
+data Deleted n k v g =
+    Deleted k (g (BTree n k v))
   | AllDeleted
   | UnChanged
 
 -- | Filter items from a 'Fixed' @('BTree' k v)@ for a key 'k' that match
 --   the predicate.
 filterBTree :: (Ord k, Fixed g) => k -> (v -> Bool) ->
-    g (BTree k v) -> g (BTree k v)
+    g (BTree n k v) -> g (BTree n k v)
 filterBTree k f t = deleted' . para phi $ t where
     deleted' UnChanged = t
     deleted' AllDeleted = empty
     deleted' (Deleted _ x) = x
+
+    nodes = fmap (\(a, (b, _)) -> (a, b))
+
     phi Empty = UnChanged
     phi (Value v) = if f v
         then UnChanged
         else AllDeleted
-    phi (Node 0 a) =
-        let al = do
-                (nk, (nn, nv)) <- elems a
-                case (nk == k, nv) of
-                    (False, _) -> return (False, ((nk, nn):))
-                    (_, UnChanged) -> return (False, ((nk, nn):))
-                    _ -> return (True, id)
-            alb = foldr ((||) . fst) False al
-            al' = foldr (($) . snd) [] al
-            mink = fst . head $ al'
-        in case (alb, null al') of
-            (True, True) -> AllDeleted
-            (True, False) -> Deleted mink $ node 0 $
-                array (0, length al' - 1) $ zip [0..] al'
-            (False, _) -> UnChanged
-    phi (Node d a) = 
-        let (p, c, n) = splitRange k a
-            p' = [(nk, nv) | (nk, (nv, _)) <- p]
-            c'' = do
-                (nk, (nn, nv)) <- c
-                case nv of
-                    UnChanged -> return (False, ((nk, nn):))
-                    AllDeleted -> return (True, id)
-                    Deleted k' v' -> return (True, ((k', v'):))
-            c' = foldr (($) . snd) [] c''
-            cb = foldr ((||) . fst) False c''
-            n' = [(nk, nv) | (nk, (nv, _)) <- n]
-            al = p' ++ c' ++ n'
-            mink = fst . head $ al
-        in case (cb, null al) of
-            (False, _) -> UnChanged
-            (True, True) -> AllDeleted
-            (True, False) -> Deleted mink $ node d $
-                array (0, length al - 1) $ zip [0..] al
+
+    phi (Node 0 vec) =
+        let (lt, eq, gt) = split3 (splitRange k vec) vec
+            lt' = nodes lt
+            gt' = nodes gt
+            (eq',del) = runWriter $ do
+                res <- flip V.filterM eq $ \(_, (_, a)) ->
+                    case a of
+                        UnChanged -> return True
+                        _ -> tell (Any True) >> return False
+                return $ nodes res
+            vec' = V.concat [lt', eq', gt']
+            mink = fst (V.head vec')
+        in case (V.null vec', getAny del) of
+            (True, _) -> AllDeleted
+            (_, False) -> UnChanged
+            _ -> Deleted mink $ node 0 vec'
+    phi (Node d vec) =
+        let (lt, eq, gt) = split3 (s1 - 1, s2) vec
+            (s1, s2) = splitRange k vec
+            lt' = nodes lt
+            gt' = nodes gt
+            (eq',del) = runWriter $ do
+                res <- flip V.filterM eq $ \(_, (_, a)) ->
+                    case a of
+                        UnChanged -> return True
+                        Deleted _ _ -> tell (Any True) >> return True
+                        AllDeleted -> tell (Any True) >> return False
+                forM res $ \(nk, (n, a)) -> do
+                    case a of
+                        UnChanged -> return (nk, n)
+                        Deleted nk' a' -> return (nk', a')
+                        AllDeleted -> error "AllDeleted?" -- should be unreachable
+            vec' = V.concat [lt', eq', gt']
+            mink = fst (V.head vec')
+        in case (V.null vec', getAny del) of
+            (True, _) -> AllDeleted
+            (_, False) -> UnChanged
+            _ -> Deleted mink $ node d vec'
 
 -- | 'Transaction' version of 'filterBTree'.
 filterBTreeT :: (Ord k, Binary k, Binary v) => k -> (v -> Bool) ->
-    Transaction (Ref (BTree k v)) s ()
+    Transaction (Ref (BTree n k v)) s ()
 filterBTreeT k f = alterT (filterBTree k f)
 
 -- | Delete all items for key 'k' from the 'Fixed' @('BTree' k v)@.
-deleteBTree :: (Ord k, Fixed g) => k -> g (BTree k v) -> g (BTree k v)
+deleteBTree :: (Ord k, Fixed g) => k -> g (BTree n k v) -> g (BTree n k v)
 deleteBTree k = filterBTree k (const False)
 
 -- | 'Transaction' version of 'deleteBTree'.
 deleteBTreeT :: (Ord k, Binary k, Binary v) => k ->
-    Transaction (Ref (BTree k v)) s ()
+    Transaction (Ref (BTree n k v)) s ()
 deleteBTreeT k = alterT (deleteBTree k)
 
+data SkewDir = L | R
+
+data Parted n k v g =
+    NoPart SkewDir
+  | Parted (k, (g (BTree n k v))) (k, (g (BTree n k v)))
+
+-- | Split a 'BTree' into two two 'BTree's with keys < 'k' and keys > 'k'.
+partitionBTree :: (Ord k, Fixed g) => k -> g (BTree n k v) ->
+    (g (BTree n k v), g (BTree n k v))
+partitionBTree k t = parted . para phi $ t where
+    parted (NoPart L) = (t, empty)
+    parted (NoPart R) = (empty, t)
+    parted (Parted (_, l) (_, r)) = (l, r)
+
+    nodes = fmap (\(a, (b, _)) -> (a, b))
+
+    phi Empty = NoPart L
+    phi (Value _) = error "Unbalanced BTree"
+    phi (Node 0 vec) =
+        let (lt, gte) = V.splitAt s1 vec
+            (s1, _) = splitRange k vec
+            minkl = fst (V.head lt)
+            minkr = fst (V.head gte)
+        in case (V.null lt, V.null gte) of
+            (True, _) -> NoPart R
+            (_, True) -> NoPart L
+            _ -> Parted (minkl, node 0 (nodes lt)) (minkr, node 0 (nodes gte))
+    phi (Node d vec) = 
+        let (lt, eq, gt) = split3 (s1 - 1, s1) vec
+            (s1, _) = splitRange k vec
+            lt' = nodes lt
+            eq' = nodes eq
+            gt' = nodes gt
+            minkl = if V.null lt then fst (V.head eq) else fst (V.head lt)
+            (_,(_,eqa)) = V.head eq
+        in case (V.null eq, V.null gt, eqa) of
+            (True, _, _) -> NoPart R
+            (_, True, NoPart L) -> NoPart L
+            (_, _, NoPart R) -> error "Malformed BTree"
+            (_, _, NoPart L) ->
+                let minkr = fst (V.head gt')
+                    ln = node d (V.concat [lt', eq'])
+                    rn = node d (V.force gt')
+                in Parted (minkl, ln) (minkr, rn)
+            (_, _, Parted tl tr@(prk, _)) ->
+                let ln = node d (V.concat [lt', V.singleton tl])
+                    rn = node d (V.concat [V.singleton tr, gt'])
+                in Parted (minkl, ln) (prk, rn)
+
 -- | Turn a 'Fixed' @('BTree' k v)@ into a list of key value tuples.
-toListBTree :: (Ord k, Fixed g) => g (BTree k v) -> [(k,v)]
+toListBTree :: (Ord k, Fixed g) => g (BTree n k v) -> [(k,v)]
 toListBTree t = cata phi t Nothing [] where
     phi Empty _ l = l
     phi (Value v) (Just k) l = (k, v):l
     phi (Value _) _ _ = error "Value with no Key"
-    phi (Node _ a) _ l = foldr (\(k,v) -> ((v (Just k)) .)) id (elems a) l
+    phi (Node _ vec) _ l = V.foldr (\(k,v) -> ((v (Just k)) .)) id vec l
 
 -- | Turn a list of key value tuples into a 'Fixed' @('BTree' k v)@.
-fromListBTree :: (Ord k, Fixed g) => [(k,v)] -> g (BTree k v)
+fromListBTree :: (KnownNat n, Ord k, Fixed g) => [(k,v)] -> g (BTree n k v)
 fromListBTree = foldr (uncurry insertBTree) empty
 
--- tests/TestBTree.hs
+++ tests/TestBTree.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE DataKinds #-}
 
 module TestBTree(testBTree) where
 
@@ -7,40 +8,60 @@ import Test.QuickCheck
 
 import Data.FixFile
 import Data.FixFile.BTree
+import qualified Data.FixFile.Tree23 as T23
 
-prop_BTreeInsert :: [[(Int, String)]] -> Bool
+prop_BTreeInsert :: [(Int, String)] -> Bool
 prop_BTreeInsert xs = allIns where
-    empt = empty :: Fix (BTree Int String)
-    xs' = concat xs
-    fullSet = foldr (uncurry insertBTree) empt xs'
-    allIns = all (not . null . flip lookupBTree fullSet) $ fmap fst xs'
+    empt = empty :: Fix (BTree 3 Int String)
+    fullSet = foldr (uncurry insertBTree) empt xs
+    allIns = all (not . null . flip lookupBTree fullSet) $ fmap fst xs
 
-prop_BTreeDelete :: [[(Int, String)]] -> [Int] -> Bool
+prop_BTreeDelete :: [(Int, String)] -> [Int] -> Bool
 prop_BTreeDelete xs ys = allIns where
-    empt = empty :: Fix (BTree Int String)
-    xs' = concat xs
-    fullSet = foldr (uncurry insertBTree) empt xs'
+    empt = empty :: Fix (BTree 3 Int String)
+    fullSet = foldr (uncurry insertBTree) empt xs
     delSet = foldr deleteBTree fullSet ys
     allIns = all (null . flip lookupBTree delSet) ys
 
-prop_BTreeDeleteAll :: [[(Int, String)]] -> Bool
+prop_BTreeDeleteAll :: [(Int, String)] -> Bool
 prop_BTreeDeleteAll xs = allIns where
-    empt = empty :: Fix (BTree Int String)
-    xs' = concat xs
-    keys = map fst xs'
-    fullSet = foldr (uncurry insertBTree) empt xs'
+    empt = empty :: Fix (BTree 3 Int String)
+    keys = map fst xs
+    fullSet = foldr (uncurry insertBTree) empt xs
     delSet = foldr deleteBTree fullSet keys
     allIns = all (null . flip lookupBTree delSet) keys
 
-prop_BTreeFilter :: [[(Int, String)]] -> Int -> String -> Bool
+prop_BTreeFilter :: [(Int, String)] -> Int -> String -> Bool
 prop_BTreeFilter xs k v = testFilt where
-    empt = empty :: Fix (BTree Int String)
-    xs' = concat xs
-    baseSet = foldr (uncurry insertBTree) empt xs'
+    empt = empty :: Fix (BTree 3 Int String)
+    baseSet = foldr (uncurry insertBTree) empt xs
     delSet = deleteBTree k baseSet
     fullSet = insertBTree k v $ insertBTree k ('a':v) delSet
     filtSet = filterBTree k (== v) fullSet
     testFilt = [v] == lookupBTree k filtSet
+
+prop_BTreePartition :: [(Int, String)] -> Int -> Bool
+prop_BTreePartition xs k = testPart where
+    empt = empty :: Fix (BTree 3 Int String)
+    fullTree = foldr (uncurry insertBTree) empt xs
+    (treeL, treeR) = partitionBTree k fullTree
+    emptT23 = T23.empty :: Fix (T23.Tree23 (T23.Map Int Int))
+    counts = T23.toListMap $ foldr countItems emptT23 xs
+    countItems (k', _) m = T23.alterMap k' (Just . maybe 1 (1+)) m
+    correctTree (k', l) =
+        let (tree1,tree2) = if k' < k
+                then (treeL, treeR)
+                else (treeR, treeL)
+        in l == (length (lookupBTree k' tree1)) &&
+            null (lookupBTree k' tree2)
+    testPart = all correctTree counts
+
+prop_BTreeNodeSize :: [(Int, String)] -> Bool
+prop_BTreeNodeSize xs = depth fullSet1 >= depth fullSet2 where
+    empt1 = empty :: Fix (BTree 2 Int String)
+    empt2 = empty :: Fix (BTree 5 Int String)
+    fullSet1 = foldr (uncurry insertBTree) empt1 xs
+    fullSet2 = foldr (uncurry insertBTree) empt2 xs
 
 testBTree = testGroup "BTree"
     [
@@ -48,4 +69,6 @@ testBTree = testGroup "BTree"
        ,testProperty "BTree Delete" prop_BTreeDelete
        ,testProperty "BTree Delete All" prop_BTreeDeleteAll
        ,testProperty "BTree Filter" prop_BTreeFilter
+       ,testProperty "BTree Partition" prop_BTreePartition
+       ,testProperty "BTree Node Size" prop_BTreeNodeSize
     ]
